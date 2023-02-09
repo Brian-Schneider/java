12fd115bfe1092f9bcf7cba7a44a948700385bca
Brian Schneider de Lima <brian.schneider.epica@gmail.com>
2023-02-09 18:04:45 -0300

    Adicionando condição de Sim ou Não ao Exercício Doador de Sangue

--- aula_02/src/aula_02/DoacaoSangue.java
+++ aula_02/src/aula_02/DoacaoSangue.java
@@ -10,22 +10,32 @@ public class DoacaoSangue {
 
 		String nomeDoador;
 		int idadeDoador;
-		boolean primDoaçao;
+		char primDoaçao;
+		boolean verificaçao = false;
 		
 		System.out.println("Digite o Nome do doador(a/e):");
 		leia.skip("\\R?");
 		nomeDoador = leia.nextLine();
 		
-		System.out.println("Digite a Idade do doador(a/e):");
+		System.out.println("\nDigite a Idade do doador(a/e):");
 		idadeDoador = leia.nextInt();
 		
-		System.out.println("Primeira vez doando sangue (digite 'false' para NÃO e 'true' para SIM)");
-		primDoaçao = leia.nextBoolean();
-
-		if (idadeDoador < 18 || idadeDoador > 69 || ((idadeDoador >= 60 && idadeDoador <= 69) && primDoaçao == true))
-			System.out.println(nomeDoador + " não está apto(a/e) para doar sangue!");
+		System.out.println("\nPrimeira vez doando sangue (digite S ou N)");
+		primDoaçao = leia.next().charAt(0);
+		
+		if (primDoaçao == 's' || primDoaçao =='S')
+			verificaçao = true;
+		else if (primDoaçao == 'n' || primDoaçao == 'N')
+			verificaçao = false;
+		else {
+			System.out.println("\nCaractere Inválido!");
+			System.exit(0);
+		}
+		
+		if (idadeDoador < 18 || idadeDoador > 69 || ((idadeDoador >= 60 && idadeDoador <= 69) && verificaçao == true))
+			System.out.println("\n"+ nomeDoador + " não está apto(a/e) para doar sangue!");
 		else
-			System.out.println(nomeDoador + " está apto(a/e)	 para doar sangue!");
+			System.out.println("\n" + nomeDoador + " está apto(a/e) para doar sangue!");
 
 		leia.close();
 
